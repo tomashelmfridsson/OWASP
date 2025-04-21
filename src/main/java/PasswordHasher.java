c582c1f7dc3b5f1337faa20957f01d0f024bd6fd
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-04-21 19:25:04 +0200

    The code

---
+++ src/main/java/PasswordHasher.java
@@ -0,0 +1,17 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordHasher {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String password = "minHemligaKod123";
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hash = md.digest(password.getBytes());
+        StringBuilder hexString = new StringBuilder();
+
+        for (byte b : hash) {
+            hexString.append(String.format("%02x", b));
+        }
+
+        System.out.println("Hashat lösenord: " + hexString.toString());
+    }
+}
